da14bff5e9f592625e4dc4f863a048b05a98c2d5
Roshan Farkate <Roshan Farkate@TCOGNITION>
2023-07-12 02:26:25 -0400

    new update

--- src/main/java/ccrpack/controller/Ccrcontroller.java
+++ src/main/java/ccrpack/controller/Ccrcontroller.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -58,9 +59,12 @@ public class Ccrcontroller {
 	@PersistenceContext
 	EntityManager entityManager;
 
+	////////////////////////////// ROSHAN /////////////////////////////////
+	
+	
 //	 @PostMapping(value ="/addcandidate")
 //     public ResponseEntity<String> createCandidate(@RequestBody Candidate candidate) {
-//         ccrService.createCandidate(candidate);
+//         ccrservice.createCandidate(candidate);
 //         return ResponseEntity.status(HttpStatus.CREATED).body("Candidate data submitted");
 //     }
 
